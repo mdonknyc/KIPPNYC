cfdfd93d8a4588bced1f368bd85dbe61d582798a
mdonknyc <mdonald@kippnyc.org>
2018-10-22 14:11:19 -0400

    Summary Added
    Summary Added to APT Data Pull

--- APT Data Pull.sql
+++ APT Data Pull.sql
@@ -1,3 +1,12 @@
+/*
+
+Title: APT Data Pull 
+Created By: Unkwown
+Last Updated: 10/22/18
+Summary: Pulls data that tracks academic progress across KIPP and NYS tests among students.
+
+*/
+											
 Select 
 S.SystemStudentID,
 S.FullName,
